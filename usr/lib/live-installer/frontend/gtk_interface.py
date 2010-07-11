d3e4407fbcc5c86adb1c39a7dc047c245b3d3d8f
Clement Lefebvre <root@linuxmint.com>
2010-07-11 08:02:51 +0100

    Bug fix and i18n in overview page

--- usr/lib/live-installer/frontend/gtk_interface.py
+++ usr/lib/live-installer/frontend/gtk_interface.py
@@ -726,31 +726,29 @@ class InstallerWindow:
 	def show_overview(self):
 		''' build the summary page '''
 		model = gtk.TreeStore(str)
-		top = model.append(None)
-		model.set(top, 0, "Filesystem operations")
-		
+				
 		top = model.append(None)
 		model.set(top, 0, _("Localization"))
 		iter = model.append(top)
-		model.set(iter, 0, "Language: <b>%s</b>" % self.locale)
+		model.set(iter, 0, _("Language: ") + "<b>%s</b>" % self.locale)
 		iter = model.append(top)
-		model.set(iter, 0, "Keyboard layout: <b>%s</b>" % self.keyboard_layout_desc)
+		model.set(iter, 0, _("Keyboard layout: ") + "<b>%s</b>" % self.keyboard_layout_desc)
 		iter = model.append(top)
-		model.set(iter, 0, "Keyboard model: <b>%s</b>" % self.keyboard_model_desc)
+		model.set(iter, 0, _("Keyboard model: ") + "<b>%s</b>" % self.keyboard_model_desc)
 				
 		top = model.append(None)
 		model.set(top, 0, _("User settings"))
 		username = self.wTree.get_widget("entry_username").get_text()
 		realname = self.wTree.get_widget("entry_your_name").get_text()	
 		iter = model.append(top)
-		model.set(iter, 0, "Real name: <b>%s</b>" % realname)	
+		model.set(iter, 0, _("Real name: ") + "<b>%s</b>" % realname)	
 		iter = model.append(top)
-		model.set(iter, 0, "Username: <b>%s</b>" % username)
+		model.set(iter, 0, _("Username: ") + "<b>%s</b>" % username)
 				
 		top = model.append(None)
 		model.set(top, 0, _("System settings"))
 		iter = model.append(top)
-		model.set(iter, 0, "Hostname: <b>%s</b>" % self.wTree.get_widget("entry_hostname").get_text())
+		model.set(iter, 0, _("Hostname: ") + "<b>%s</b>" % self.wTree.get_widget("entry_hostname").get_text())
 				
 		install_grub = self.wTree.get_widget("checkbutton_grub").get_active()
 		grub_box = self.wTree.get_widget("combobox_grub")
@@ -758,20 +756,22 @@ class InstallerWindow:
 		grub_model = grub_box.get_model()
 		iter = model.append(top)
 		if(install_grub):
-			model.set(iter, 0, _("Install bootloader to %s" % ("<b>%s</b>" % grub_model[grub_active][0])))
+			model.set(iter, 0, _("Install bootloader in %s") % ("<b>%s</b>" % grub_model[grub_active][0]))
 		else:
 			model.set(iter, 0, _("Do not install bootloader"))
 				
+		top = model.append(None)
+		model.set(top, 0, _("Filesystem operations"))
 		disks = self.wTree.get_widget("treeview_disks").get_model()
 		for item in disks:
 			if(item[2]):
 				# format it
 				iter = model.append(top)
-				model.set(iter, 0, "<b>%s</b>" % (_("Format %s (%s) as %s" % (item[0], item[4], item[1]))))
+				model.set(iter, 0, "<b>%s</b>" % (_("Format %s (%s) as %s") % (item[0], item[4], item[1])))
 			if(item[3] is not None and item[3] is not ""):
 				# mount point
 				iter = model.append(top)
-				model.set(iter, 0, "<b>%s</b>" % (_("Mount %s as %s" % (item[0], item[3]))))
+				model.set(iter, 0, "<b>%s</b>" % (_("Mount %s as %s") % (item[0], item[3])))
 		
 		self.wTree.get_widget("treeview_overview").set_model(model)
 		
