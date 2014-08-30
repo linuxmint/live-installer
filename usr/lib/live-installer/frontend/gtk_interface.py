d7eec8f02942a50b3dcdbf439e7919502795c673
Kernc <kerncece@gmail.com>
2014-08-30 22:53:17 +0200

    Simplified timezone map pixel calculation

--- usr/lib/live-installer/frontend/gtk_interface.py
+++ usr/lib/live-installer/frontend/gtk_interface.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+from __future__ import division
 from installer import InstallerEngine, Setup, PartitionSetup
 from slideshow import Slideshow
 
@@ -150,40 +150,15 @@ class Timezone:
         
     # @return pixel coordinate of a latitude and longitude for self
     # map uses Miller Projection, but is also clipped
-    def getPosition(self, la, lo):
-        # need to add/sub magic numbers because the map doesn't actually go from -180...180, -90...90
-        # thus the upper corner is not -180, -90 and we have to compensate
-        # we need a better method of determining the actually range so we can better place citites (shtylman)
-        xdeg_offset = -6
-        # the 180 - 35) accounts for the fact that the map does not span the entire -90 to 90
-        # the map does span the entire 360 though, just offset
-        x = (self.width * (180.0 + lo) / 360.0) + (self.width * xdeg_offset/ 180.0)
-        x = x % self.width
-
-        #top and bottom clipping latitudes
-        topLat = 81
-        bottomLat = -59
-
-        #percent of entire possible range
-        topPer = topLat/180.0
-
-        # get the y in rectangular coordinates
-        y = 1.25 * math.log(math.tan(math.pi/4.0 + 0.4 * math.radians(la)))
-
-        # calculate the map range (smaller than full range because the map is clipped on top and bottom
-        fullRange = 4.6068250867599998
-        # the amount of the full range devoted to the upper hemisphere
-        topOffset = fullRange*topPer
-        mapRange = abs(1.25 * math.log(math.tan(math.pi/4.0 + 0.4 * math.radians(bottomLat))) - topOffset)
-
-        # Convert to a percentage of the map range
-        y = abs(y - topOffset)
-        y = y / mapRange
-
-        # this then becomes the percentage of the height
-        y = y * self.height
-
-        return (int(x), int(y))        
+    MAP_CENTER = (373, 261)  # pixel center of where equatorial line and 0th meridian cross on our bg map
+    MAP_SIZE = (800, 409)    # size of the map image
+    def getPosition(self, lat, lon):
+        dx = self.MAP_SIZE[0] / 2 / 180
+        dy = self.MAP_SIZE[1] / 2 / 90
+        # formulae from http://en.wikipedia.org/wiki/Miller_cylindrical_projection
+        x = self.MAP_CENTER[0] + dx * lon
+        y = self.MAP_CENTER[1] - dy * math.degrees(5/4 * math.log(math.tan(math.pi/4 + 2/5 * math.radians(lat))))
+        return int(x), int(y)
         
 class InstallerWindow:
     # Cancelable timeout for keyboard preview generation, which is
