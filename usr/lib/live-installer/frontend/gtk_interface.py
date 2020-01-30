a2951989a6900cca76154d5fadcf22a3793c300d
Michael Webster <miketwebster@gmail.com>
2020-01-29 22:28:16 -0500

    gtk_interface.py: Use a spinning widget instead of a gif when loading a keyboard layout.

--- usr/lib/live-installer/frontend/gtk_interface.py
+++ usr/lib/live-installer/frontend/gtk_interface.py
@@ -646,10 +646,14 @@ class InstallerWindow:
             self.setup.print_setup()
 
         # Set preview image
-        self.builder.get_object("image_keyboard").set_from_file(LOADING_ANIMATION)
-        self.kbd_preview_generation = GObject.timeout_add(500, self._generate_keyboard_layout_preview)
+        self.builder.get_object("image_keyboard").hide()
+        self.builder.get_object("kb_spinner").show()
+        self.builder.get_object("kb_spinner").start()
 
-    def _generate_keyboard_layout_preview(self):
+        self._generate_keyboard_layout_preview()
+
+    @idle
+    def _generate_keyboard_layout_preview(self, *args, **kwargs):
         filename = "/tmp/live-install-keyboard-layout.png"
         layout = self.setup.keyboard_layout.split(",")[-1]
         variant = self.setup.keyboard_variant.split(",")[-1]
@@ -657,8 +661,18 @@ class InstallerWindow:
             variant = None
         print("python /usr/lib/live-installer/frontend/generate_keyboard_layout.py %s %s %s" % (layout, variant, filename))
         os.system("python /usr/lib/live-installer/frontend/generate_keyboard_layout.py %s %s %s" % (layout, variant, filename))
+        self._on_layout_generated()
+
+    @idle
+    def _on_layout_generated(self):
+        filename = "/tmp/live-install-keyboard-layout.png"
+
+        self.builder.get_object("kb_spinner").stop()
+        self.builder.get_object("kb_spinner").hide()
 
         widget = self.builder.get_object("image_keyboard")
+        widget.show()
+
         try:
             pixbuf = GdkPixbuf.Pixbuf.new_from_file(filename)
             surface = Gdk.cairo_surface_create_from_pixbuf(pixbuf, widget.get_scale_factor(), widget.get_window())
