9e9b42c1da5c352ff29badcc468f66071a9fe949
Adınız <siz@e-posta.com>
2021-02-10 22:55:11 +0000

    debian script fil

--- live-installer/scripts/fetch-pkg-debian.sh
+++ live-installer/scripts/fetch-pkg-debian.sh
@@ -1,13 +1,14 @@
 fetch_deb(){
     mkdir -p /target/debs/ || true
     for pkg in $@ ; do
-        cp -pvf $(find /run/live/medium/pool/ -iname $pkg_*.deb) /target/debs/
+        cp -pvf $(find /run/live/medium/pool/ -iname ${pkg}_*.deb) /target/debs/
     done
-    chroot /target dpkg -i /debs
-    rm -rf /debs
+    chroot /target sh -c "dpkg -i /debs/*"
+    rm -rf /target/debs
 }
 
 fetch_deb "efibootmgr" "grub-common" "grub-efi-amd64" "grub-efi-amd64-bin" \
     "grub-efi-amd64-signed" "grub2-common" "libefiboot1" "libefivar1" \
     "mokutil" "os-prober" "shim-helpers-amd64-signed" "shim-signed" \
     "shim-signed-common" "shim-unsigned" "grub-pc-bin"
+
