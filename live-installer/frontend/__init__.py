8b8cd2c33605e7c5fbb2b99299d206929c91668f
Muhammed Efe Çetin <efectn@protonmail.com>
2021-08-12 23:37:19 +0300

    fix

--- live-installer/frontend/__init__.py
+++ live-installer/frontend/__init__.py
@@ -5,6 +5,7 @@ import time
 import math
 import re
 from utils import *
-from gi.repository import Gtk, Gdk, GdkPixbuf, GObject, Pango, GLib
 import gi
 gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gdk, GdkPixbuf, GObject, Pango, GLib
+
