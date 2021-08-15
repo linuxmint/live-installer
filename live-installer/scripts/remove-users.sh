1511d0e160095086c5f5b2af4b414d23187c9345
Ali Rıza KESKİN <parduscix@yandex.ru>
2021-08-15 13:55:59 +0300

    arch user remove fix

---
+++ live-installer/scripts/remove-users.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+grep [0-9][0-9][0-9][0-9] /target/etc/passwd | while read line ; do
+    user=$(echo $line | cut -f 1 -d ":")
+    home=$(echo $line | cut -f 6 -d ":")
+    if [[ "$user" != "" ]] ; then
+        chroot /target deluser "$user"
+        [[ -d /target/$home ]] && rm -rf /target/$home 
+    fi
+done
